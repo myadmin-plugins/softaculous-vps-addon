69ada7c280755eac537997e7948bc75a7953e2f0
Joe Huss <detain@interserver.net>
2017-12-19 18:52:45 -0500

    adding function and requirements for function to handle adding the addon

---
+++ src/vps_add_softaculous.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * VPS Functionality
+ * @author Joe Huss <detain@interserver.net>
+ * @copyright 2017
+ * @package MyAdmin
+ * @category VPS
+ */
+
+/**
+ * Adds Softaculous to a VPS
+ * @return void
+ */
+function vps_add_softaculous() {
+	function_requirements('class.AddServiceAddon');
+	$addon = new AddServiceAddon();
+	$addon->load(__FUNCTION__, 'Softaculous', 'vps', VPS_SOFTACULOUS_COST);
+	$addon->process();
+}
